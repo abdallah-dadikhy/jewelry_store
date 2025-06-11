686c9fb8149dc2fb57820465b204bc0abb8faf85
abdallah dadikhy <abdallahdadikhy@gmail.com>
2025-06-11 14:30:02 +0300

    ProductFile

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -20,7 +20,7 @@ class ProductResource extends JsonResource
             'decription'=>$this->Description,
             'weight'=>$this->Weight,
             'price'=>$this->Price,
-            'ProductFile' => $this->ProductFile ? asset('storage/' . $this->ProductFile) : null,
+            'productFile' => $this->ProductFile ? asset('storage/' . $this->ProductFile) : null,
             'isFeatured'=>$this->IsFeatured,
             'categoryID'=>$this->CategoryID,
         ];
